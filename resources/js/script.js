05a46150f200d505528b433785678422b190f2dc
Dcrueda34 <dacarol19@hotmail.com>
2022-09-07 10:14:13 -0500

    modificacion de random, style y partida

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,10 +1,10 @@
-let cards = ['aire', 'fuego', 'tierra', 'agua'];
-let numbers = ['5', '10', '15', '20', '25', '30', '35', '40', '45', '50', '55', '60', '65'];
+let cards = ['AIRE'];
+let numbers = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
 
 
 function displayCard() {
   let singleCard = cards[Math.floor(Math.random() * cards.length)];
   let showCard = `${singleCard}`;
-  document.getElementById("A1").style.background = '#0accff';
+  document.getElementById("cardshowing").style.background = '#0accff';
   document.getElementById("A1").innerHTML = showCard;
 }
