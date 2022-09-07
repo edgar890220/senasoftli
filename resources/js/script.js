efcb14c04d93e346da85dc224afbb56771f52241
edgar890220 <winner.11.lr@gmail.com>
2022-09-07 15:13:14 -0500

    modificacion

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,12 +1,18 @@
-c  = cards[i];
-cards[i]=
-
-
-
+let cards = ['♥', '♣', '♦', '♠'];
+let numbers = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+let imagenes=new Array(
+    'img/fondo.png',
+    'img/play.png'
+);
 
 function displayCard() {
   let singleCard = cards[Math.floor(Math.random() * cards.length)];
-  let showCard = `${singleCard}`;
-  document.getElementById("cardshowing").style.background = '#0accff';
-  document.getElementById("A1").innerHTML = showCard;
+  let singleNumber = numbers[Math.floor(Math.random() * numbers.length)];
+  let singleImagen=imagenes[Math.floor((Math.random()*imagenes.length))];
+  let showCard = `${singleNumber}   ${singleCard} `;
+  document.getElementById("showing").innerHTML = showCard;
+  document.getElementById("imagen").src=imagenes;
+
 }
+
+
--- resources/views/jugador/form.blade.php
+++ resources/views/jugador/form.blade.php
@@ -9,26 +9,10 @@
       <td>{!! $errors->first('email', '<div class="invalid-feedback">:message</div>') !!}</td>
     </tr>
 
+    <tr><div class="box-footer mt20">
+        <td><center><br><br><button type="submit" class="btn btn-primary" style="WIDTH: 40%; HEIGHT: 32px;  COLOR: white;
+            BACKGROUND-COLOR: black"><font color=yellow> CREAR</button></center></td>
+      </div></tr>
 
-
-
-
-
-            <table >
-
-          <tr > <td> {{ Form::text('nombre', $jugador->nombre, ['class' => 'form-control' . ($errors->has('nombre') ? ' is-invalid' : ''), 'placeholder' => 'Digite su Nombre']) }}</td>
-            <td >{!! $errors->first('nombre', '<div class="invalid-feedback">:message</div>') !!}</td>
-          </tr> <br><br>
-          <tr>
-
-           <td> {{ Form::text('email', $jugador->email, ['class' => 'form-control' . ($errors->has('email') ? ' is-invalid' : ''),  'placeholder' => 'Digite su Email']) }}</td>
-            <td>{!! $errors->first('email', '<div class="invalid-feedback">:message</div>') !!}</td>
-          </tr>
-
-
-<tr><div class="box-footer mt20">
-  <td><center><br><br><button type="submit" class="btn btn-primary" style="WIDTH: 40%; HEIGHT: 32px;  COLOR: white;
-      BACKGROUND-COLOR: black"><font color=yellow> CREAR</button></center></td>
-</div></tr>
 
 </table>
